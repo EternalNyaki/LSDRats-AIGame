dffa7c12272d53f7109ab6e6871da8fe2264315b
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2025-03-04 16:00:07 -0500

    Added character movement

---
+++ AI Escape Game/Assets/Scripts/RPGMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPGMovement : MonoBehaviour
+{
+    //Movement speed (in units per second)
+    public float moveSpeed;
+
+    //Vector for storing inputs
+    private Vector2 _inputVector;
+
+    private Rigidbody2D _rb2d;
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        _animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+
+        _inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        _animator.SetFloat("Speed", _inputVector.magnitude * moveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        _rb2d.MovePosition((Vector2)transform.position + _inputVector * moveSpeed * Time.deltaTime);
+    }
+}
